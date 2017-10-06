1c49e6cc84484199c5b8450469950626f0bfd22e
Robert JONES <rojones@e4r16p1.wethinkcode.co.za>
2017-10-06 22:15:30 +0200

    init

---
+++ Hotline Miami/Assets/Scripts/HumanContller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HumanContller : MonoBehaviour {
+	[SerializeField]
+	private float _speed;
+	public float ShowFps
+	{
+		get { return _speed; }
+		set
+		{_speed = value; }
+	}
+
+	private Rigidbody2D rb2d;
+	private Animator	anim;
+
+	void Awake()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	public void move(Vector2 dir)
+	{
+		dir.Normalize ();
+		if (dir != new Vector2 (0, 0)) {
+			anim.SetBool ("Walk", true);
+		} else {
+			anim.SetBool ("Walk", false);
+		}
+		rb2d.velocity = dir * _speed;
+	}
+}
---
+++ Hotline Miami/Assets/Scripts/PlyerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlyerController : HumanContller {
+
+	void Update()
+	{
+		float h = Input.GetAxis ("Vertical");
+		float w = Input.GetAxis ("Horizontal");
+		move (new Vector2 (w, h));
+	}
+}
